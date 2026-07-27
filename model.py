e5e6aa128f513a5aaa24e02392b4672ad6273e95
Samkit Jain <samkit77jain@gmail.com>
2026-07-27 23:49:15 +0530

    Step 13: pad_2d

--- model.py
+++ model.py
@@ -95,8 +95,11 @@ def init_zero_bias(length):
     # TODO: return a 1D float array of zeros with the given length.
     return np.zeros(length, dtype=np.float64)
 
-# Step 13 - pad_2d (not yet solved)
-# TODO: implement
+# Step 13 - pad_2d
+def pad_2d(images, pad):
+    # TODO: zero-pad the spatial (H, W) dims of a 4D (N, C, H, W) tensor by `pad` on each side.
+    pad_width = ((0, 0), (0,0),(pad, pad), (pad, pad))
+    return np.pad(images, pad_width, mode='constant', constant_values=0)
 
 # Step 14 - output_spatial_size (not yet solved)
 # TODO: implement
