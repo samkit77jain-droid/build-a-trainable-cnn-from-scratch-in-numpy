34c4fa4971d245c4f923bca52805e866be07e9c2
Samkit Jain <samkit77jain@gmail.com>
2026-07-31 22:46:25 +0530

    Step 14: output_spatial_size

--- model.py
+++ model.py
@@ -103,7 +103,7 @@ def pad_2d(images, pad):
 
 # Step 14 - output_spatial_size
 def output_spatial_size(input_size, kernel, stride, padding):
-    # TODO: return the conv/pool output spatial dimension from input_size, kernel, stride, padding
+    
     return (input_size - kernel + 2 * padding) // stride + 1
 
 # Step 15 - im2col
