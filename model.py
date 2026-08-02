0288ddf397c86ec22864009adac7d8e48de24456
Samkit Jain <samkit77jain@gmail.com>
2026-08-02 20:35:32 +0530

    Step 41: adam_step

--- model.py
+++ model.py
@@ -424,8 +424,19 @@ def adam_bias_correct(moment, beta, t):
 def adam_param_step(param, m_hat, v_hat, lr, eps):
     return param - lr * m_hat / (np.sqrt(v_hat) + eps)
 
-# Step 41 - adam_step (not yet solved)
-# TODO: implement
+# Step 41 - adam_step
+import numpy as np
+
+def adam_step(param, grad, m, v, t, lr, beta_one, beta_two, eps):
+    new_m = adam_update_m(m, grad, beta_one)
+    new_v = adam_update_v(v, grad, beta_two)
+
+    m_hat = adam_bias_correct(new_m, beta_one, t)
+    v_hat = adam_bias_correct(new_v, beta_two, t)
+
+    new_param = adam_param_step(param, m_hat, v_hat, lr, eps)
+    
+    return new_param, new_m, new_v
 
 # Step 42 - init_conv_layer (not yet solved)
 # TODO: implement
