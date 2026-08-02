ab1b79e977576a8c7c118dc954662c01159834a0
Samkit Jain <samkit77jain@gmail.com>
2026-08-02 11:28:27 +0530

    Step 21: conv2d_backward

--- model.py
+++ model.py
@@ -223,8 +223,13 @@ def conv2d_grad_bias(d_out):
     
     return db
 
-# Step 21 - conv2d_backward (not yet solved)
-# TODO: implement
+# Step 21 - conv2d_backward
+def conv2d_backward(d_out, cache):
+    dx = conv2d_grad_input(d_out, cache)
+    dW = conv2d_grad_weights(d_out, cache)
+    db = conv2d_grad_bias(d_out)
+    
+    return dx, dW, db
 
 # Step 22 - maxpool2d_forward (not yet solved)
 # TODO: implement
