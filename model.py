48f765ff3c72c803ecbcfbf7e05c5eae3efb98b4
Samkit Jain <samkit77jain@gmail.com>
2026-08-03 21:57:04 +0530

    Step 50: lenet_backward

--- model.py
+++ model.py
@@ -606,8 +606,27 @@ def backward_classifier_block(dlogits, cache):
         'fc2': {'dW': dW2, 'db': db2},
     }
 
-# Step 50 - lenet_backward (not yet solved)
-# TODO: implement
+# Step 50 - lenet_backward
+def lenet_backward(dlogits, caches):
+    # 1. Backprop through the classifier block
+    grads_cls = backward_classifier_block(dlogits, caches['classifier'])
+    dout2 = grads_cls['dx']
+
+    # 2. Backprop through conv block 2
+    dout1, dW2, db2 = backward_conv_block(dout2, caches['block2'])
+
+    # 3. Backprop through conv block 1
+    dx, dW1, db1 = backward_conv_block(dout1, caches['block1'])
+
+    # Assemble all gradients into a single dictionary
+    grads = {
+        'conv1': {'dW': dW1, 'db': db1},
+        'conv2': {'dW': dW2, 'db': db2},
+        'fc1': grads_cls['fc1'],
+        'fc2': grads_cls['fc2'],
+    }
+    
+    return grads
 
 # Step 51 - lenet_predict (not yet solved)
 # TODO: implement
